2dcae97befd5ea328527d405b5c193d2dd8a97c4
Lucas-Louback <lucasmlouback99@gmail.com>
2023-03-16 16:22:04 -0300

    exercícios corrigidos

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let numeroPar = Number( prompt("Insira um número par") )
+console.log(`O número escolhido foi ${numeroPar}`)
+let divisao = Number(numeroPar % 2)
+console.log(`Esse número dividido por dois tem resto: ${Number(divisao)}`)
+//todos os números pares divididos por dois tem resto 0
+//todos os numeros ímpares tem resto 1
